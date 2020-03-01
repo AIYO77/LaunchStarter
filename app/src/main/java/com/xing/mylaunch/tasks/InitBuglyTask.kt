17875514a245c2981edc1ff4dbb14dd70fac5234
xingwei <xingwei@znds.com>
2020-03-01 10:21:48 +0800

    去除无用资源

--- app/src/main/java/com/xing/mylaunch/tasks/InitBuglyTask.kt
+++ app/src/main/java/com/xing/mylaunch/tasks/InitBuglyTask.kt
@@ -1,5 +1,6 @@
 package com.xing.mylaunch.tasks
 
+import android.util.Log
 import com.tencent.bugly.crashreport.CrashReport
 import com.xing.launchstarter.task.Task
 
@@ -12,5 +13,11 @@ import com.xing.launchstarter.task.Task
 class InitBuglyTask: Task() {
     override fun run() {
         CrashReport.initCrashReport(mContext, "注册时申请的APPID", false)
+    }
+
+    override fun getTailRunnable(): Runnable? {
+        return Runnable {
+            Log.d("task","${this::class.java.simpleName} 结束")
+        }
     }
 }
